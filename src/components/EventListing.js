db79d93d4e347397702ab8bfdcd0ab74446c5048
Nicky Hajal <nhajal@gmail.com>
2018-05-18 16:46:22 -0500

    count in listing

--- src/components/EventListing.js
+++ src/components/EventListing.js
@@ -9,23 +9,48 @@ const Table = styled.table`
   border-collapse: collapse;
 `;
 const Row = styled.tr`
+  &.heading {
+    border-bottom: 2px solid rgb(210, 222, 221);
+    background: rgb(235, 243, 239);
+    border-top: 25px solid #fff;
+  }
   td {
     padding: 12px 10px;
     text-align: left;
     cursor: pointer;
+    opacity: 0.9;
     &:first-of-type {
       font-weight: 600;
     }
-    opacity: 0.9;
+    &.data {
+      text-align: center;
+    }
   }
   &:hover {
     td {
       opacity: 1;
     }
   }
+  &:first-of-type {
+    border-top: 0;
+  }
+`;
+const Head = styled.thead`
+  padding: 0;
+
+  th,
+  tr {
+    margin: 0;
+    padding: 4px;
+    text-align: center;
+  }
 `;
 
-const EventRow = ({ event: { what, who, hosts, start }, even, onClick }) => {
+const EventRow = ({
+  event: { type, what, who, hosts, start, num_free, num_rsvps, max, free_max },
+  even,
+  onClick,
+}) => {
   return (
     <Row
       onClick={onClick}
@@ -35,18 +60,18 @@ const EventRow = ({ event: { what, who, hosts, start }, even, onClick }) => {
     >
       <td style={{ width: '90px' }}>{moment.utc(start).format('h:mm a')}</td>
       <td>{what}</td>
+      {type === 'academy' && (
+        <td className="data">{`${num_free}/${free_max}`}</td>
+      )}
+      {['academy', 'activity', 'meetup'].includes(type) && (
+        <td className="data">{`${num_rsvps}/${max}`}</td>
+      )}
     </Row>
   );
 };
 const Heading = ({ heading }) => {
   return (
-    <Row
-      style={{
-        borderBottom: `2px solid rgb(210, 222, 221)`,
-        backgroundColor: 'rgb(235, 243, 239)',
-        borderTop: `25px solid #fff`,
-      }}
-    >
+    <Row className="heading">
       <td colspan="30" style={{ fontSize: '18px', fontWeight: '700' }}>
         {heading}
       </td>
@@ -63,8 +88,28 @@ const EventListing = ({ events, onClick }) => {
     out.push(curr);
     return out;
   }, []);
+  const type = events[0].type;
   return (
     <Table>
+      {['meetup', 'activity'].includes(type) && (
+        <Head>
+          <tr>
+            <th />
+            <th />
+            <th>RSVPs</th>
+          </tr>
+        </Head>
+      )}
+      {type === 'academy' && (
+        <Head>
+          <tr>
+            <th />
+            <th />
+            <th>Free</th>
+            <th>Paid</th>
+          </tr>
+        </Head>
+      )}
       <tbody>
         {eventsWithHeadings.map(
           (e, i) =>
