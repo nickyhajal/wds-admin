c8e47ae9d8666ede7f63486ca7ca06786bd561f7
Nicky Hajal <nhajal@gmail.com>
2018-05-25 20:44:00 -0500

    avatar

---
+++ src/components/Avatar.js
@@ -0,0 +1,26 @@
+import React from 'react';
+import styled from 'styled-components';
+
+const Avatar = styled.div`
+  background-size: cover;
+  position: relative;
+  display: inline-block;
+  box-shadow: 5px 5px 12px rgba(0, 0, 0, 0.12);
+`;
+
+export default ({ user_id, user, width, style }) => {
+  if (user) {
+    user_id = user.user_id;
+  }
+  const url = `url(https://avatar.wds.fm/${user_id}?width=${width * 2})`;
+  const dim = `${width}px`;
+  console.log(url);
+  return (
+    <Avatar
+      style={Object.assign(
+        { backgroundImage: url, width: dim, height: dim },
+        style,
+      )}
+    />
+  );
+};
