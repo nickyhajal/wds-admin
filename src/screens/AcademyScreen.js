35f37fdb4b8cdea6604467a24580a6d962584dbc
Nicky Hajal <nhajal@gmail.com>
2022-04-15 07:16:22 -0500

    dates

--- src/screens/AcademyScreen.js
+++ src/screens/AcademyScreen.js
@@ -1,39 +1,39 @@
-import React from 'react';
-import styled from 'styled-components';
-import { withRouter } from 'react-router-dom';
-import { Tab, Tabs, TabList, TabPanel } from 'react-tabs';
-import { Base64 } from 'js-base64';
-import Select from 'react-select';
-import moment from 'moment';
-import query from '../util/query';
-import Container from '../components/Container';
-import Form from '../components/Form';
-import FormRow from '../components/FormRow';
-import Input from '../components/Input';
-import AddressForm from '../components/AddressForm';
-import Colors from '../constants/Colors';
-import Table from '../components/Table';
-import TicketsTable from '../components/TicketsTable';
-import TransactionsTable from '../components/TransactionsTable';
-import Label from '../components/Label';
-import apollo from '../util/apollo';
-import mutateAddTicket from '../graph/mutateAddTicket';
-import SubmitButton from '../components/SubmitButton';
-import queryUser from '../graph/queryUser';
-import UserAdminNoteContainer from '../containers/UserAdminNoteContainer';
-import Textarea from '../components/Textarea';
-import TimeSelect from '../components/TimeSelect';
-import DateSelect from '../components/DateSelect';
-import Search from '../containers/Search';
-import AttendeeSearch from '../containers/AttendeeSearch';
-import mutateUpdateEvent from '../graph/mutateUpdateEvent';
+import React from 'react'
+import styled from 'styled-components'
+import {withRouter} from 'react-router-dom'
+import {Tab, Tabs, TabList, TabPanel} from 'react-tabs'
+import {Base64} from 'js-base64'
+import Select from 'react-select'
+import moment from 'moment'
+import query from '../util/query'
+import Container from '../components/Container'
+import Form from '../components/Form'
+import FormRow from '../components/FormRow'
+import Input from '../components/Input'
+import AddressForm from '../components/AddressForm'
+import Colors from '../constants/Colors'
+import Table from '../components/Table'
+import TicketsTable from '../components/TicketsTable'
+import TransactionsTable from '../components/TransactionsTable'
+import Label from '../components/Label'
+import apollo from '../util/apollo'
+import mutateAddTicket from '../graph/mutateAddTicket'
+import SubmitButton from '../components/SubmitButton'
+import queryUser from '../graph/queryUser'
+import UserAdminNoteContainer from '../containers/UserAdminNoteContainer'
+import Textarea from '../components/Textarea'
+import TimeSelect from '../components/TimeSelect'
+import DateSelect from '../components/DateSelect'
+import Search from '../containers/Search'
+import AttendeeSearch from '../containers/AttendeeSearch'
+import mutateUpdateEvent from '../graph/mutateUpdateEvent'
 
-const Page = styled.div``;
+const Page = styled.div``
 
 const ColContent = styled.div`
   display: flex;
   align-items: flex-start;
-`;
+`
 const ContentSide = styled.div`
   background: ${Colors.white};
   padding: 32px;
@@ -42,7 +42,7 @@ const ContentSide = styled.div`
   box-shadow: 3px 3px 20px rgba(0, 0, 0, 0.08);
   flex: 1;
   margin-top: 110px;
-`;
+`
 
 const RemoveButton = styled.button`
   margin-left: 7px;
@@ -54,11 +54,11 @@ const RemoveButton = styled.button`
   padding: 2px 15px;
   color: #444;
   border-radius: 3px;
-`;
+`
 
 class AcademyScreen extends React.Component {
   constructor() {
-    super();
+    super()
     this.state = {
       giveTicket: 'y',
       status: 'ready',
@@ -90,120 +90,122 @@ class AcademyScreen extends React.Component {
         price: null,
         hosts: [],
       },
-    };
-  }
-  changeType = e => {
-    this.setState({
-      user: Object.assign({}, this.state.user, { type: e.value }),
-    });
-  };
-  change = e => {
-    if (e.currentTarget.name !== undefined) {
-      const { name, value } = e.currentTarget;
-      this.upd(name, value);
     }
-  };
+  }
+  changeType = (e) => {
+    this.setState({
+      user: Object.assign({}, this.state.user, {type: e.value}),
+    })
+  }
+  change = (e) => {
+    if (e.currentTarget.name !== undefined) {
+      const {name, value} = e.currentTarget
+      this.upd(name, value)
+    }
+  }
   upd = (name, value) => {
     this.setState({
-      event: Object.assign({}, this.state.event, { [name]: value }),
-    });
-  };
+      event: Object.assign({}, this.state.event, {[name]: value}),
+    })
+  }
   componentWillReceiveProps(props) {
-    let e = Object.assign({}, props.data.event);
-    const start = moment(e.start);
-    const end = moment(e.end);
-    const date = start.format('DD');
-    let hour = +start.format('HH');
-    let end_hour = +end.format('HH');
-    let pm = '0';
-    let end_pm = '0';
+    let e = Object.assign({}, props.data.event)
+    const start = moment(e.start)
+    const end = moment(e.end)
+    const date = start.format('DD')
+    let hour = +start.format('HH')
+    let end_hour = +end.format('HH')
+    let pm = '0'
+    let end_pm = '0'
     if (+hour >= 12) {
-      if (+hour > 12) hour -= 12;
-      pm = '12';
+      if (+hour > 12) hour -= 12
+      pm = '12'
     }
-    if (+hour < 10) hour = '0' + hour;
-    if (hour === '00') hour = '12';
+    if (+hour < 10) hour = '0' + hour
+    if (hour === '00') hour = '12'
     if (+end_hour >= 12) {
       if (+end_hour > 12) {
-        end_hour -= 12;
+        end_hour -= 12
       }
-      end_pm = '12';
+      end_pm = '12'
     }
-    if (+end_hour < 10) end_hour = '0' + end_hour;
-    if (end_hour === '00') end_hour = '12';
-    e.date = date;
-    e.hour = hour.toString();
-    e.end_hour = end_hour.toString();
-    e.minute = start.format('mm');
-    e.ampm = pm;
-    e.end_minute = end.format('mm');
-    e.end_ampm = end_pm;
+    if (+end_hour < 10) end_hour = '0' + end_hour
+    if (end_hour === '00') end_hour = '12'
+    e.date = date
+    e.hour = hour.toString()
+    e.end_hour = end_hour.toString()
+    e.minute = start.format('mm')
+    e.ampm = pm
+    e.end_minute = end.format('mm')
+    e.end_ampm = end_pm
     const bios = e.bios
       ? JSON.parse(e.bios.includes('{') ? e.bios : Base64.decode(e.bios))
-      : {};
+      : {}
     this.setState({
       bios,
       event: Object.assign({}, e),
-    });
+    })
   }
-  save = async e => {
-    e.preventDefault();
-    const event = Object.assign({}, this.state.event);
-    event.bios = Base64.encode(JSON.stringify(this.state.bios));
-    event.hosts = event.hosts.map(v => v.user_id).join(',');
-    this.setState({ status: 'saving' });
+  save = async (e) => {
+    e.preventDefault()
+    const event = Object.assign({}, this.state.event)
+    event.bios = Base64.encode(JSON.stringify(this.state.bios))
+    event.hosts = event.hosts.map((v) => v.user_id).join(',')
+    this.setState({status: 'saving'})
     const res = await apollo.mutate({
       mutation: mutateUpdateEvent,
       variables: event,
-    });
-    this.setState({ status: 'success' });
+    })
+    this.setState({status: 'success'})
     setTimeout(() => {
-      this.setState({ status: 'ready' });
-    }, 2000);
-  };
+      this.setState({status: 'ready'})
+    }, 2000)
+  }
   changeStart = (part, value) => {
     this.setState({
-      event: Object.assign({}, this.state.event, { [part]: value }),
-    });
-  };
+      event: Object.assign({}, this.state.event, {[part]: value}),
+    })
+  }
   changeEnd = (part, value) => {
-    this.changeStart(`end_${part}`, value);
-  };
-  addHost = host => {
-    const hosts = [...this.state.event.hosts];
-    if (hosts.findIndex(({ user_id }) => user_id === host.user_id) === -1) {
-      hosts.push(host);
+    this.changeStart(`end_${part}`, value)
+  }
+  addHost = (host) => {
+    const hosts = [...this.state.event.hosts]
+    if (hosts.findIndex(({user_id}) => user_id === host.user_id) === -1) {
+      hosts.push(host)
     }
-    this.upd('hosts', hosts);
-  };
+    this.upd('hosts', hosts)
+  }
   updateBio = (user_id, e) => {
     if (e.currentTarget.name !== undefined) {
-      const { name, value } = e.currentTarget;
-      const bios = this.state.bios;
-      bios[user_id] = value;
-      this.setState({ bios });
+      const {name, value} = e.currentTarget
+      const bios = this.state.bios
+      bios[user_id] = value
+      this.setState({bios})
     }
-  };
-  removeHost = filter_id => {
-    const hosts = [...this.state.event.hosts];
-    this.upd('hosts', hosts.filter(({ user_id }) => user_id !== filter_id));
-  };
+  }
+  removeHost = (filter_id) => {
+    const hosts = [...this.state.event.hosts]
+    this.upd(
+      'hosts',
+      hosts.filter(({user_id}) => user_id !== filter_id)
+    )
+  }
   render() {
     const dates = [
-      { label: 'June 26th', value: '26' },
-      { label: 'June 27th', value: '27' },
-      { label: 'June 28th', value: '28' },
-      { label: 'June 29th', value: '29' },
-      { label: 'July 1st', value: '1' },
-      { label: 'July 2nd', value: '2' },
-      { label: 'July 3rd', value: '3' },
-    ];
+      {label: 'June 22nd', value: '22'},
+      {label: 'June 23rd', value: '23'},
+      {label: 'June 24th', value: '24'},
+      {label: 'June 25th', value: '25'},
+      {label: 'June 26th', value: '26'},
+      {label: 'June 27th', value: '27'},
+    ]
     return (
       <div>
         {this.props.data.loading ? (
           <div>Loading</div>
         ) : (
-          <ColContent style={{ marginBottom: '200px' }}>
+          <ColContent style={{marginBottom: '200px'}}>
             <div>
               <h2>{this.state.event.what}</h2>
               <div className="react-tabs__tab-panel react-tabs__tab-panel--selected">
@@ -290,7 +292,7 @@ class AcademyScreen extends React.Component {
                         value={this.state.event.date}
                         options={dates}
                         clearable={false}
-                        onChange={e => this.changeStart('date', e.value)}
+                        onChange={(e) => this.changeStart('date', e.value)}
                       />
                     </div>
                     <div>
@@ -316,7 +318,7 @@ class AcademyScreen extends React.Component {
                   </FormRow>
                   <h3>Hosts</h3>
                   <FormRow col={1}>
-                    <div style={{ flex: 1 }}>
+                    <div style={{flex: 1}}>
                       <div>
                         <AttendeeSearch onSelect={this.addHost} />
                       </div>
@@ -329,7 +331,7 @@ class AcademyScreen extends React.Component {
                         }}
                       >
                         {this.state.event.hosts.map(
-                          ({ first_name, last_name, user_id }) => (
+                          ({first_name, last_name, user_id}) => (
                             <div
                               style={{
                                 display: 'flex',
@@ -364,7 +366,7 @@ class AcademyScreen extends React.Component {
                                     Remove
                                   </RemoveButton>
                                 </div>
-                                <div style={{ display: 'flex' }}>
+                                <div style={{display: 'flex'}}>
                                   <div
                                     style={{
                                       width: '48px',
@@ -382,12 +384,12 @@ class AcademyScreen extends React.Component {
                                       borderTopLeftRadius: '0',
                                     }}
                                     value={this.state.bios[user_id]}
-                                    onChange={e => this.updateBio(user_id, e)}
+                                    onChange={(e) => this.updateBio(user_id, e)}
                                   />
                                 </div>
                               </div>
                             </div>
-                          ),
+                          )
                         )}
                       </div>
                     </div>
@@ -408,12 +410,12 @@ class AcademyScreen extends React.Component {
           </ColContent>
         )}
       </div>
-    );
+    )
   }
 }
 
 export default withRouter(
-  query('event', AcademyScreen, ({ match }) => ({
-    variables: { event_id: match.params.id },
-  })),
-);
+  query('event', AcademyScreen, ({match}) => ({
+    variables: {event_id: match.params.id},
+  }))
+)
--- src/screens/AddAcademyScreen.js
+++ src/screens/AddAcademyScreen.js
@@ -1,41 +1,41 @@
-import React from 'react';
-import styled from 'styled-components';
-import { withRouter } from 'react-router-dom';
-import { Tab, Tabs, TabList, TabPanel } from 'react-tabs';
-import { Base64 } from 'js-base64';
-import Select from 'react-select';
-import moment from 'moment';
-import query from '../util/query';
-import Container from '../components/Container';
-import Form from '../components/Form';
-import FormRow from '../components/FormRow';
-import Input from '../components/Input';
-import AddressForm from '../components/AddressForm';
-import Colors from '../constants/Colors';
-import Table from '../components/Table';
-import TicketsTable from '../components/TicketsTable';
-import TransactionsTable from '../components/TransactionsTable';
-import Label from '../components/Label';
-import apollo from '../util/apollo';
-import mutateAddTicket from '../graph/mutateAddTicket';
-import SubmitButton from '../components/SubmitButton';
-import queryUser from '../graph/queryUser';
-import mutateAddUser from '../graph/mutateAddUser';
-import UserAdminNoteContainer from '../containers/UserAdminNoteContainer';
-import Textarea from '../components/Textarea';
-import TimeSelect from '../components/TimeSelect';
-import DateSelect from '../components/DateSelect';
-import Search from '../containers/Search';
-import AttendeeSearch from '../containers/AttendeeSearch';
-import mutateAddEvent from '../graph/mutateAddEvent';
-import RemoveButton from '../components/RemoveButton';
+import React from 'react'
+import styled from 'styled-components'
+import {withRouter} from 'react-router-dom'
+import {Tab, Tabs, TabList, TabPanel} from 'react-tabs'
+import {Base64} from 'js-base64'
+import Select from 'react-select'
+import moment from 'moment'
+import query from '../util/query'
+import Container from '../components/Container'
+import Form from '../components/Form'
+import FormRow from '../components/FormRow'
+import Input from '../components/Input'
+import AddressForm from '../components/AddressForm'
+import Colors from '../constants/Colors'
+import Table from '../components/Table'
+import TicketsTable from '../components/TicketsTable'
+import TransactionsTable from '../components/TransactionsTable'
+import Label from '../components/Label'
+import apollo from '../util/apollo'
+import mutateAddTicket from '../graph/mutateAddTicket'
+import SubmitButton from '../components/SubmitButton'
+import queryUser from '../graph/queryUser'
+import mutateAddUser from '../graph/mutateAddUser'
+import UserAdminNoteContainer from '../containers/UserAdminNoteContainer'
+import Textarea from '../components/Textarea'
+import TimeSelect from '../components/TimeSelect'
+import DateSelect from '../components/DateSelect'
+import Search from '../containers/Search'
+import AttendeeSearch from '../containers/AttendeeSearch'
+import mutateAddEvent from '../graph/mutateAddEvent'
+import RemoveButton from '../components/RemoveButton'
 
-const Page = styled.div``;
+const Page = styled.div``
 
 const ColContent = styled.div`
   display: flex;
   align-items: flex-start;
-`;
+`
 const ContentSide = styled.div`
   background: ${Colors.white};
   padding: 32px;
@@ -44,11 +44,11 @@ const ContentSide = styled.div`
   box-shadow: 3px 3px 20px rgba(0, 0, 0, 0.08);
   flex: 1;
   margin-top: 110px;
-`;
+`
 
 class AddEventScreen extends React.Component {
   constructor() {
-    super();
+    super()
     this.state = {
       status: 'ready',
       bios: {},
@@ -79,79 +79,81 @@ class AddEventScreen extends React.Component {
         price: null,
         hosts: [],
       },
-    };
-  }
-  changeType = e => {
-    this.setState({
-      user: Object.assign({}, this.state.user, { type: e.value }),
-    });
-  };
-  change = e => {
-    if (e.currentTarget.name !== undefined) {
-      const { name, value } = e.currentTarget;
-      this.upd(name, value);
     }
-  };
+  }
+  changeType = (e) => {
+    this.setState({
+      user: Object.assign({}, this.state.user, {type: e.value}),
+    })
+  }
+  change = (e) => {
+    if (e.currentTarget.name !== undefined) {
+      const {name, value} = e.currentTarget
+      this.upd(name, value)
+    }
+  }
   upd = (name, value) => {
     this.setState({
-      event: Object.assign({}, this.state.event, { [name]: value }),
-    });
-  };
-  componentWillReceiveProps(props) {
-    this.setState({ event: Object.assign({}, props.data.event) });
+      event: Object.assign({}, this.state.event, {[name]: value}),
+    })
   }
-  startAdd = async e => {
-    e.preventDefault();
-    const event = Object.assign({}, this.state.event);
-    event.bios = Base64.encode(JSON.stringify(this.state.bios));
-    event.hosts = event.hosts.map(v => v.user_id).join(',');
-    this.setState({ status: 'saving' });
+  componentWillReceiveProps(props) {
+    this.setState({event: Object.assign({}, props.data.event)})
+  }
+  startAdd = async (e) => {
+    e.preventDefault()
+    const event = Object.assign({}, this.state.event)
+    event.bios = Base64.encode(JSON.stringify(this.state.bios))
+    event.hosts = event.hosts.map((v) => v.user_id).join(',')
+    this.setState({status: 'saving'})
     const res = await apollo.mutate({
       mutation: mutateAddEvent,
       variables: event,
-    });
-    const { event_id } = res.data.eventAdd;
-    this.setState({ status: 'success' });
-    setTimeout(() => this.props.history.push(`/academy/${event_id}`), 1000);
-    setTimeout(() => window.scrollTo(0, 0), 1100);
-  };
+    })
+    const {event_id} = res.data.eventAdd
+    this.setState({status: 'success'})
+    setTimeout(() => this.props.history.push(`/academy/${event_id}`), 1000)
+    setTimeout(() => window.scrollTo(0, 0), 1100)
+  }
   changeStart = (part, value) => {
     this.setState({
-      event: Object.assign({}, this.state.event, { [part]: value }),
-    });
-  };
+      event: Object.assign({}, this.state.event, {[part]: value}),
+    })
+  }
   changeEnd = (part, value) => {
-    this.changeStart(`end_${part}`, value);
-  };
-  addHost = host => {
-    const hosts = this.state.event.hosts;
-    hosts.push(host);
-    this.upd('hosts', hosts);
-  };
+    this.changeStart(`end_${part}`, value)
+  }
+  addHost = (host) => {
+    const hosts = this.state.event.hosts
+    hosts.push(host)
+    this.upd('hosts', hosts)
+  }
   updateBio = (user_id, e) => {
     if (e.currentTarget.name !== undefined) {
-      const { name, value } = e.currentTarget;
-      const bios = this.state.bios;
-      bios[user_id] = value;
-      this.setState({ bios });
+      const {name, value} = e.currentTarget
+      const bios = this.state.bios
+      bios[user_id] = value
+      this.setState({bios})
     }
-  };
-  removeHost = filter_id => {
-    const hosts = this.state.event.hosts;
-    this.upd('hosts', hosts.filter(({ user_id }) => user_id !== filter_id));
-  };
+  }
+  removeHost = (filter_id) => {
+    const hosts = this.state.event.hosts
+    this.upd(
+      'hosts',
+      hosts.filter(({user_id}) => user_id !== filter_id)
+    )
+  }
   render() {
     const dates = [
-      { label: 'June 26th', value: '26' },
-      { label: 'June 27th', value: '27' },
-      { label: 'June 28th', value: '28' },
-      { label: 'June 29th', value: '29' },
-      { label: 'July 1st', value: '1' },
-      { label: 'July 2nd', value: '2' },
-      { label: 'July 3rd', value: '3' },
-    ];
+      {label: 'June 22nd', value: '22'},
+      {label: 'June 23rd', value: '23'},
+      {label: 'June 24th', value: '24'},
+      {label: 'June 25th', value: '25'},
+      {label: 'June 26th', value: '26'},
+      {label: 'June 27th', value: '27'},
+    ]
     return (
-      <ColContent style={{ marginBottom: '200px' }}>
+      <ColContent style={{marginBottom: '200px'}}>
         <div>
           <h2>Add an Academy</h2>
           <div className="react-tabs__tab-panel react-tabs__tab-panel--selected">
@@ -238,7 +240,7 @@ class AddEventScreen extends React.Component {
                     value={this.state.event.date}
                     options={dates}
                     clearable={false}
-                    onChange={e => this.changeStart('date', e.value)}
+                    onChange={(e) => this.changeStart('date', e.value)}
                   />
                 </div>
                 <div>
@@ -264,7 +266,7 @@ class AddEventScreen extends React.Component {
               </FormRow>
               <h3>Hosts</h3>
               <FormRow col={1}>
-                <div style={{ flex: 1 }}>
+                <div style={{flex: 1}}>
                   <div>
                     <AttendeeSearch onSelect={this.addHost} />
                   </div>
@@ -277,7 +279,7 @@ class AddEventScreen extends React.Component {
                     }}
                   >
                     {this.state.event.hosts.map(
-                      ({ first_name, last_name, user_id }) => (
+                      ({first_name, last_name, user_id}) => (
                         <div
                           style={{
                             display: 'flex',
@@ -304,12 +306,12 @@ class AddEventScreen extends React.Component {
                               {`${first_name} ${last_name}`}
                               <RemoveButton
                                 onClick={() => this.removeHost(user_id)}
-                                style={{ marginLeft: '4px', marginTop: '-3px' }}
+                                style={{marginLeft: '4px', marginTop: '-3px'}}
                               >
                                 Remove
                               </RemoveButton>
                             </div>
-                            <div style={{ display: 'flex' }}>
+                            <div style={{display: 'flex'}}>
                               <div
                                 style={{
                                   width: '48px',
@@ -322,14 +324,14 @@ class AddEventScreen extends React.Component {
                               />
                               <Textarea
                                 placeholder={`${first_name}'s Bio...`}
-                                style={{ flex: 1, borderTopLeftRadius: '0' }}
+                                style={{flex: 1, borderTopLeftRadius: '0'}}
                                 value={this.state.bios[user_id]}
-                                onChange={e => this.updateBio(user_id, e)}
+                                onChange={(e) => this.updateBio(user_id, e)}
                               />
                             </div>
                           </div>
                         </div>
-                      ),
+                      )
                     )}
                   </div>
                 </div>
@@ -348,8 +350,8 @@ class AddEventScreen extends React.Component {
           </div>
         </div>
       </ColContent>
-    );
+    )
   }
 }
 
-export default withRouter(AddEventScreen);
+export default withRouter(AddEventScreen)
