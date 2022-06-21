4885fe7a56f3a0c4613d028d673263e30ac087df
Nicky Hajal <nhajal@gmail.com>
2022-06-21 10:12:23 -0700

    fix: notification event select

--- src/components/EventSelect.js
+++ src/components/EventSelect.js
@@ -1,9 +1,9 @@
-import React from 'react';
-import Select from 'react-select';
-import moment from 'moment';
-import query from '../util/query';
+import React from 'react'
+import Select from 'react-select'
+import moment from 'moment'
+import query from '../util/query'
 
-const EventSelect = ({ data, name, onChange, value }) => {
+const EventSelect = ({data, name, onChange, value}) => {
   return data.loading ? (
     <div>Loading...</div>
   ) : (
@@ -11,14 +11,16 @@ const EventSelect = ({ data, name, onChange, value }) => {
       name={name}
       value={value}
       onChange={onChange}
-      options={data.events.filter(e => e.max > 0).map(e => ({
-        value: e.event_id,
-        label: `[${e.type}] ${e.what}`,
-      }))}
+      options={data.events
+        .filter((e) => e.max > 0)
+        .map((e) => ({
+          value: e.event_id,
+          label: `[${e.type}] ${e.what}`,
+        }))}
     />
-  );
-};
+  )
+}
 
-export default query('bareEvents', EventSelect, ({ match }) => ({
-  variables: { year: moment().format('YY'), orderBy: 'type ASC, start ASC' },
-}));
+export default query('bareEvents', EventSelect, ({match}) => ({
+  variables: {year: '20', orderBy: 'type ASC, start ASC'},
+}))
